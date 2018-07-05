cf38afc01f994ed7ef981837cde483deee0c2a4f
DarthHater <jeffryxtron@gmail.com>
2018-07-05 11:17:09 -0800

    Fix up merge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import config from './config'
 import store from './store/index';
 import { loadThreads } from './actions/threadActions';
 
