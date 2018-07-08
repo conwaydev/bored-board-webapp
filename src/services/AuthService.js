2dcd2c77c5e9a9c9c7725a3f6f739e17c526b7f5
DarthHater <jeffryxtron@gmail.com>
2018-07-07 16:23:14 -0800

    Axios

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,5 +1,6 @@
 import * as auth from '../auth/authentication';
 import config from 'react-global-configuration';
+import axios from 'axios';
 
 export const authService = {
     login,
@@ -8,18 +9,24 @@ export const authService = {
 
 function login(data) {
     let baseUrl = config.get('API_ROOT');
-    return fetch(`${baseUrl}/login`, {
-        method: 'POST',
+
+    return axios.post(`${baseUrl}/login`, data, {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-        },
-        body: data
+        }  
         })
-        .then(handleResponse)
-        .then(user => {
-            if (user.token) {
-                sessionStorage.setItem('jwt', user.token);
+        .then(function(response) {
+            if (!response.ok) {
+                if (response.status === 401) {
+                    // auto logout if 401 response returned from api
+                    logout();
+                    location.reload(true);
+                } else {
+                    if (response.data.token) {
+                        sessionStorage.setItem('jwt', response.data.token);
+                    }
+                }
             }
         })
         .catch(error => {
@@ -29,22 +36,4 @@ function login(data) {
 
 function logout() {
     auth.logOut();
-}
-
-function handleResponse(response) {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if (response.status === 401) {
-                // auto logout if 401 response returned from api
-                logout();
-                location.reload(true);
-            }
- 
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
-        }
- 
-        return data;
-    });
 }
